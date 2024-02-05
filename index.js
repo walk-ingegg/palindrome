402a2e46f076e8f2c9626f1fee130e7cc6c10905
walk-ingegg <walk-ingegg@omuken.me>
2024-02-05 18:29:45 +0900

    update script and add test

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // reverse()メソッドを追加してすべてのStringで使えるようにする
 String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
@@ -9,7 +11,20 @@ function Phrase(content) {
 
   // パリンドロームのテスト用に変換した訳文を返す
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  };
+
+  // コンテンツの文字だけを返す
+  // 利用例:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    for (let i = 0; i < this.content.length; i++) {
+      if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+        theLetters.push(this.content.charAt(i));
+      }
+    }
+    return theLetters.join("");
   };
 
   // パリンドロームならtrueを、違うならfalseを返す
---
+++ test/test.js
@@ -0,0 +1,33 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function () {
+  describe("#palindrome", function () {
+    it("パリンドロームでない場合はfalseを返す", function () {
+      let nonPalindrome = new Phrase("apple");
+      assert.strictEqual(nonPalindrome.palindrome(), false);
+    });
+
+    it("単純なパリンドロームの場合はtrueを返す", function () {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("パリンドロームが大文字小文字混じりでもtrueを返す", function () {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("パリンドロームに句読点が含まれていてもtrueを返す", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  });
+
+  describe("#letters", function () {
+    it("文字列だけを返す", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
+  });
+});
